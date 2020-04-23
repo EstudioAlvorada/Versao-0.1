7ea93c8eea29aacbe8d7d44a8029917d2686a7f0
Yuukilari <larissaaguiar80@gmail.com>
2020-04-22 21:16:42 -0300

    Iniciando a criacao do menu

---
+++ Assets/MenuPause.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPause : MonoBehaviour
+{
+    public static bool JogoPausado = false;
+
+    public GameObject pausarMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (JogoPausado)
+            {
+                Retomar();
+            }
+            else
+            {
+                Pausar();
+            }
+        }
+    }
+
+    void Retomar()
+    {
+        pausarMenu.SetActive(false);
+        Time.timeScale = 1f;
+        JogoPausado = false;
+    }
+
+    void Pausar()
+    {
+        pausarMenu.SetActive(true);
+        Time.timeScale = 0f;
+        JogoPausado = true;
+    }
+
+    public void IrParaMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Sair()
+    {
+        Debug.Log("Saindo");
+        Application.Quit();
+    }
+}
---
+++ Assets/MenuPrincipal.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    #region Iniciar e Sair
+    public void IniciarJogo()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void EscolherSala()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Sair()
+    {
+        Debug.Log("Saindo");
+        Application.Quit();
+    }
+    #endregion
+
+    #region Volume
+    public AudioMixer audioMixer;
+
+    public void AlterarVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+    #endregion
+}
