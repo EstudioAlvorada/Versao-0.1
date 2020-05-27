f1feec61a28cf0f2fb89b0dad2d5b90842d7dba1
Marcus Primo <marcus.primov@gmail.com>
2020-05-27 08:12:59 -0300

    Moedas

--- Assets/Scripts/Chegada.cs
+++ Assets/Scripts/Chegada.cs
@@ -38,6 +38,7 @@ public class Chegada : MonoBehaviour
 
         }
     }
+    
 
     IEnumerator FimDeJogoVencedor()
     {
---
+++ Assets/Scripts/Moedas.cs
@@ -0,0 +1,43 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Moedas : MonoBehaviour
+{
+
+
+    TextMeshProUGUI textoMoedas;
+    PhotonView photonView;
+    private int contMoeda = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        textoMoedas = GameObject.FindGameObjectWithTag("TextoMoedas").GetComponent<TextMeshProUGUI>();
+        photonView = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (photonView.IsMine)
+        {
+            if (other.gameObject.tag == "Moeda")
+            {
+                Debug.Log("Pegou a Moeda");
+                contMoeda++;
+                textoMoedas.text = (contMoeda).ToString();
+
+                Destroy(other.gameObject);
+            }
+        }
+       
+    }
+}
